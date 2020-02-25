6bdcd18566c6fa4f6d343911435d078b2b4bc4a9
MartinK <martinsingh91@gmail.com>
2020-02-25 17:10:39 +0100

    Save point. Updating DB

--- ModelView/AchievementViewModel.cs
+++ ModelView/AchievementViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,14 @@ namespace MyResume.WebApp.ModelView
 {
     public class AchievementViewModel
     {
+        [Required]
         public string Title { get; set; }
-
+        [Required]
         public string Summary { get; set; }
-     
+        [Required]
         public string MainText { get; set; }
+
+        [Display(Name = "Order position")]
         public int OrderPosition { get; set; }
         public bool EnableComments { get; set; }
         public bool EnableRating { get; set; }
