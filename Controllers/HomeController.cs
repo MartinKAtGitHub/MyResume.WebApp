187dcf1e59679acbea6c5085ff99528ee1596387
MartinK <martinsingh91@gmail.com>
2020-03-04 20:03:29 +0100

    setting up a gallery style file upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -301,23 +301,28 @@ namespace MyResume.WebApp.Controllers
             if (ImageFile != null)
             {
                 var maxFileSize = Convert.ToInt32(_config.GetSection("FileUploadSettings")["MaxFileSize"]);
+                
+                if(ImageFile.Length == 0)
+                {
+                    ModelState.AddModelError("", $"File is empty");
+                    return imageFilePath;
+                }
+                
                 if (ImageFile.Length > maxFileSize)
                 {
-                    ModelState.AddModelError("", $"Max file size allowed is {maxFileSize / 1000} KB");
+                    ModelState.AddModelError("", $"Max file size allowed is {maxFileSize / 1000} KB"); // This can also be 1024
 
                     return imageFilePath;
                 }
 
-                var fileExtention = Path.GetExtension(ImageFile.FileName);
+                var fileExtention = Path.GetExtension(ImageFile.FileName).ToLower();
 
-                if (!fileExtention.Equals(".png"))
+                if (!fileExtention.Equals(".png")) // this might be faked, should read the file signature bytes of the file to confirm its a PNG file
                 {
 
                     ModelState.AddModelError("", "Only PNG images are supported");
 
-
                     return imageFilePath;
-
                 }
 
                 string uploadsFolderPath = Path.Combine(_webHostEnvironment.WebRootPath, storageFilePath); // This will find the storage folder in wwwroot
@@ -334,10 +339,20 @@ namespace MyResume.WebApp.Controllers
 
                 using (var fileStream = new FileStream(FilePath, FileMode.Create))
                 {
+                    if(fileStream.Length == 0)
+                    {
+
+                    }
+
                     ImageFile.CopyTo(fileStream);
                 }
 
                 imageFilePath = $"~/{storageFilePath}/{imageName}";
+            }
+
+            if(imageFilePath != null) 
+            {
+                ViewBag.FileSuccessMessage = " New avatar image was successfully uploaded"; // idk if this is a good idea.
             }
 
             return imageFilePath;
