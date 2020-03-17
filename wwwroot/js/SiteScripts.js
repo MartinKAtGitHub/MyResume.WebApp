6039241ea017fd4c11e9695a137f069bc7165bce
MartinK <martinsingh91@gmail.com>
2020-03-17 20:35:58 +0100

    Css, finished confirm deletion prompt.

--- wwwroot/js/SiteScripts.js
+++ wwwroot/js/SiteScripts.js
@@ -3,19 +3,28 @@
 function confirmDelete(uniqueId, isDeleteClicked) {
     var deleteSpan = 'deleteSpan_' + uniqueId;
     var confirmDeleteSpan = 'confirmDeleteSpan_' + uniqueId;
-    var editBtn = 'editBtn_' + uniqueId;
+  //  var editBtn = 'editBtn_' + uniqueId;
     var editCol = 'editCol_' + uniqueId;
+    var confirmTxt = 'confirmTxt_' + uniqueId;
+    var titleTxt = 'titleTxt_' + uniqueId;
 
     if (isDeleteClicked) {
         $('#' + deleteSpan).hide();
-        $('#' + editBtn).hide();
+    //    $('#' + editBtn).hide();
         $('#' + editCol).hide();
+        $('#' + titleTxt).hide();
+
         $('#' + confirmDeleteSpan).show();
+        $('#' + confirmTxt).show();
     }
     else {
         $('#' + deleteSpan).show();
-        $('#' + editBtn).show();
+      //  $('#' + editBtn).show();
         $('#' + editCol).show();
+        $('#' + titleTxt).show();
+
         $('#' + confirmDeleteSpan).hide();
+        $('#' + confirmTxt).hide();
+
     }
 }
